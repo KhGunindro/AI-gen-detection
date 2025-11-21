6221010192c05c3c7dd7ecc2b64815b4f090c1c2
IndiaAIlab <ailab2@DESKTOP-5B0JLP7.localdomain>
2025-11-21 15:51:25 +0530

    Tweaked cnn_model.py

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -1,6 +1,7 @@
 from keras import layers, models
 
-def create_model(input_shape, num_classes):
+def create_model(image_size, num_classes):
+    input_shape = (image_size, image_size, 3)
     model = models.Sequential()
     
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
